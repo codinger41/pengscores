bb2fb0bfcf00d7b19a0e5d371548446d0eaa4490
Olamilekan Ibrahim <leksyib14@gmail.com>
2019-09-23 10:45:57 +0100

    Update axios.ts

--- app/utils/axios.ts
+++ app/utils/axios.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: 'https://api-football-v1.p.rapidapi.com/v2',
   headers: {
     'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
-    'x-rapidapi-key': '5088ed9f42mshdb8f74ace323105p1fcdefjsn82b3f2bcc65e'
+    'x-rapidapi-key': '<INSERT API KEY HERE>'
   }
 })
 
